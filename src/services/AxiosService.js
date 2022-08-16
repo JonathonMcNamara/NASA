484297834934b01f28cca902cac747cb67dc4d39
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-08-16 15:04:53 -0600

    updated

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -4,3 +4,11 @@ export const api = Axios.create({
   baseURL,
   timeout: 8000
 })
+
+export const nasaApi = Axios.create({
+  baseURL: 'https://api.nasa.gov/planetary/apod',
+  timeout: 4000,
+  params: {
+    api_key: 'NLgA6aC7KFu5YEdxRgy8bLugNT6DeUynZhTPS3ZQ'
+  }
+})
